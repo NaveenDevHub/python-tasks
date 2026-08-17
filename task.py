b71cb936459942ee677f40bc6e8a981fbc854b8e
NaveenDevHub <zynkcodesolutions@gmail.com>
2026-08-17 18:09:21 +0530

    third commit

--- task.py
+++ task.py
@@ -16,10 +16,11 @@ C = 5/9 * (a - 32)
 
 print("Celcius value is", C , "C")
 
-
-'''Write a Python program to find area of trianlge
+'''
+Write a Python program to find area of trianlge
 area = sqrt(s *(s-a)*(s-b)*(s-c))
 where s = a+b+c /2
+
 '''
 
 # method 1
@@ -47,5 +48,13 @@ print("The area of Triangle : ",area)
 
 # Write a python program to display the ASCII value of a given character.
 
+
+
 for i in range(65,90):
     print(chr(i))
+
+
+a='N'
+print(ord(a))
+
+# ord means print a --> 65
